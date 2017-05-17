768f6f63ccaec00d442d66e9070d180435855be9
zhangjun <zj@aek56.com>
2017-05-17 17:35:25 +0800

    delete repository/UserRepository.java

--- src/main/java/com/aek56/microservice/auth/security/repository/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.aek56.microservice.auth.security.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.aek56.microservice.auth.model.security.User;
-
-
-public interface UserRepository extends JpaRepository<User, Long> {
-    User findByUsername(String username);
-}
