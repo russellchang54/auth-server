cf364da8b3186d639563a6e90a9fc0930a0a7187
zhaogz <zgz@aek56.com>
2017-05-22 16:55:13 +0800

    obj

--- src/main/java/com/aek56/microservice/auth/model/security/TokenInfo.java
+++ src/main/java/com/aek56/microservice/auth/model/security/TokenInfo.java
@@ -1,9 +1,6 @@
 package com.aek56.microservice.auth.model.security;
 
 import java.util.Date;
-import java.util.List;
-
-import com.aek56.microservice.auth.entity.SysTenant;
 
 public class TokenInfo {
 	private String token;
@@ -11,8 +8,6 @@ public class TokenInfo {
 	private Long tanentId;/* 当前租户 */
 
 	private Long inTanentId;/* 所属租户 */
-
-	private List<SysTenant> sysTenants; /* 可用租户列表 */
 
 	private String deviceType;
 
@@ -22,14 +17,6 @@ public class TokenInfo {
 
 	public void setInTanentId(Long inTanentId) {
 		this.inTanentId = inTanentId;
-	}
-
-	public List<SysTenant> getSysTenants() {
-		return sysTenants;
-	}
-
-	public void setSysTenants(List<SysTenant> sysTenants) {
-		this.sysTenants = sysTenants;
 	}
 
 	private Date loginTime;
