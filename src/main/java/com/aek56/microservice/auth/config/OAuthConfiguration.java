c6854b78512124950bf90d7c12ae8c022b1baa1d
zhangjun@yantuchina.com <zhangjun@yantuchina.com>
2017-06-11 01:03:45 +0800

    OAuthConfiguration

--- src/main/java/com/aek56/microservice/auth/config/OAuthConfiguration.java
+++ src/main/java/com/aek56/microservice/auth/config/OAuthConfiguration.java
@@ -12,6 +12,9 @@ import org.springframework.core.io.ClassPathResource;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.authentication.configurers.GlobalAuthenticationConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
@@ -24,6 +27,11 @@ import org.springframework.security.oauth2.provider.token.store.JdbcTokenStore;
 import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
 import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
 import org.springframework.security.oauth2.provider.token.store.KeyStoreKeyFactory;
+import org.springframework.stereotype.Service;
+
+import com.aek56.microservice.auth.entity.SysUser;
+import com.aek56.microservice.auth.model.security.AuthUserFactory;
+import com.aek56.microservice.auth.security.service.SystemService;
 
 
 @Configuration
@@ -34,16 +42,18 @@ public class OAuthConfiguration extends AuthorizationServerConfigurerAdapter {
 	private AuthenticationManager auth;
 
 	//@Autowired
-//	private DataSource dataSource;
+	private DataSource dataSource;
 
-//	private BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+	private BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
 
 
 	/*@Bean
 	public JdbcTokenStore tokenStore_jdbc() {
 		return new JdbcTokenStore(dataSource);
 	}*/
-
+    @Autowired
+    private UserDetailsService userDetailsService;
+    
 	@Bean 
 	public JwtTokenStore  tokenStore() {
 		return new JwtTokenStore(jwtAccessTokenConverter());
@@ -59,27 +69,41 @@ public class OAuthConfiguration extends AuthorizationServerConfigurerAdapter {
 		return converter;
 	}
 
-/*	@Bean
+	@Bean
 	protected AuthorizationCodeServices authorizationCodeServices() {//验证码认证
 		return new JdbcAuthorizationCodeServices(dataSource);
-	}*/
-
-/*	@Override
-	public void configure(AuthorizationServerSecurityConfigurer security)
-			throws Exception {
-		security.passwordEncoder(passwordEncoder);
-		
-	}*/
+	}
 
 	@Override
-	public void configure(AuthorizationServerEndpointsConfigurer endpoints)
+	public void configure(AuthorizationServerSecurityConfigurer security)
 			throws Exception {
-		//endpoints.authorizationCodeServices(authorizationCodeServices())
-		endpoints.authenticationManager(auth).tokenStore(tokenStore())
+		security.passwordEncoder(passwordEncoder)//加密client secret
+		        .tokenKeyAccess("permitAll()"); //开发接口/oauth/token_key。 client 可以获取公钥，以做验签
+		
+		
+	}
+
+	@Override
+	public void configure(AuthorizationServerEndpointsConfigurer endpoints)//配置Authorization Server 的token授予方式(Grant Types)
+			throws Exception {
+		endpoints.authenticationManager(auth)//密码授予token
+		         .authorizationCodeServices(authorizationCodeServices())//授权码授予token
+		         .userDetailsService(userDetailsService) //通过refresh token授予的token时，检测userdetails,确保账户是active
+		         .tokenStore(tokenStore())
+		         .pathMapping("/oauth/token", "/auth/token")
 				.approvalStoreDisabled();
 	}
 
-
+/*几个endpoint: 默认都是protected
+ * /oauth/authorize 授权endpoint
+ * /oauth/token 签发token endpoint。如果被保护了，必须提供 client secret 才能访问此token
+ * /oauth/confirm_access 
+ * /oauth/error
+ * /oauth/check_token 资源服务器 decode access token
+ * /oauth/token_key
+ * (non-Javadoc)
+ * @see org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter#configure(org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer)
+ */
 	@Override
 	public void configure(ClientDetailsServiceConfigurer clients)
 			throws Exception {
@@ -101,9 +125,10 @@ public class OAuthConfiguration extends AuthorizationServerConfigurerAdapter {
 						"refresh_token","password", "implicit");
 		      
 	
-	}
+	}	
+	
 
-/*	@Configuration
+	@Configuration
 	@Order(Ordered.LOWEST_PRECEDENCE - 20)
 	protected static class AuthenticationManagerConfiguration extends
 			GlobalAuthenticationConfigurerAdapter {
@@ -120,7 +145,31 @@ public class OAuthConfiguration extends AuthorizationServerConfigurerAdapter {
 					.password("ebey").roles("ADMIN");
                         auth.jdbcAuthentication().dataSource(dataSource).withUser("russell").password("aek56").roles("ADMIN");
 			// @formatter:on
+		}	
+		
+	}
+	
+	@Service
+	public class JwtUserDetailsServiceImpl implements UserDetailsService {
+
+		/**
+		 * 系统服务
+		 */
+		@Autowired
+		private SystemService systemService;
+
+		@Override
+		public UserDetails loadUserByUsername(String loginName) {
+			SysUser user = systemService.getUserByLoginName(loginName);
+
+			if (user == null) {
+				throw new UsernameNotFoundException(String.format("No user found with username '%s'.", loginName));
+			}
+
+			Long tenantId = user.getTenantId();
+			systemService.getPerms(user, tenantId);
+			return AuthUserFactory.create(user);
 		}
-	}*/
+	}
 	
 }
